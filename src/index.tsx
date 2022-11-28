a5704203d141741d42aba69b3a90e62b7d4e35ea
@Mike-Prybytkin <prybytkin@bk.ru>
2022-11-29 01:55:55 +0300

    refactor: add material UI library

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './style/index.scss';
 import App from './components/app/App';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
