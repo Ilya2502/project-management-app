116e0a7cc6f4a9ad90a1ad347015677639d0bd41
Ilya2502 <gavran2502@mail.ru>
2022-11-29 11:31:24 +0300

    refactor: remove commits in index

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './style/index.scss';
 import App from './components/app/App';
-// import { getAllUsers, createUser, loginUser } from 'components/service/user-service/user-service';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
@@ -11,11 +10,3 @@ root.render(
     <App />
   </BrowserRouter>
 );
-
-// const checkToken = async () => {
-//   createUser('gavr3', 'gavr3', '12345678');
-//   await loginUser('gavr', '12345678');
-//   getAllUsers();
-// };
-
-// checkToken();
