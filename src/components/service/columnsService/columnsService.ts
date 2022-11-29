e5c228a61a74666371d7a4bac8a6bab034cf9f15
Ilya2502 <gavran2502@mail.ru>
2022-11-29 20:07:40 +0300

    feat: add columnsService

--- src/components/service/columnsService/columnsService.ts
+++ src/components/service/columnsService/columnsService.ts
@@ -1,1 +1,62 @@
 import { getData, postData, deleteData, putData } from '../fetchService/fetchService';
+import { ColumnsInBoardResponseType, NewColumnType, ColumnResponseType } from './types';
+import { getLocalStorageUserId } from '../localStorageService/localStorageService';
+
+const createColumn = async (boardId: string, title: string, order: number) => {
+  const endPoint = `boards/${boardId}/columns`;
+  const body = { title, order };
+  const data = await postData<ColumnResponseType, NewColumnType>(endPoint, body);
+  console.log(data);
+  return data;
+};
+
+const getColumnsInBoard = async (boardId: string) => {
+  const endPoint = `boards/${boardId}/columns`;
+  const data = await getData<ColumnsInBoardResponseType[]>(endPoint);
+  console.log(data);
+  return data;
+};
+
+const getColumnById = async (boardId: string, columnId: string) => {
+  const endPoint = `boards/${boardId}/columns/${columnId}`;
+  const data = await getData<ColumnResponseType>(endPoint);
+  console.log(data);
+  return data;
+};
+
+const getColumnsByUserId = async () => {
+  const userId = getLocalStorageUserId();
+  const endPoint = `columnsSet?userId=${userId}`;
+  const data = await getData<ColumnResponseType[]>(endPoint);
+  console.log(data);
+  return data;
+};
+
+const deleteColumnById = async (boardId: string, columnId: string) => {
+  const endPoint = `boards/${boardId}/columns/${columnId}`;
+  const data = await deleteData(endPoint);
+  console.log(data);
+  return data;
+};
+
+const updateColumnById = async (
+  boardId: string,
+  columnId: string,
+  title: string,
+  order: number
+) => {
+  const endPoint = `boards/${boardId}/columns/${columnId}`;
+  const body = { title, order };
+  const data = await putData<ColumnResponseType, NewColumnType>(endPoint, body);
+  console.log(data);
+  return data;
+};
+
+export const columnsService = {
+  getColumnsInBoard,
+  createColumn,
+  getColumnById,
+  updateColumnById,
+  deleteColumnById,
+  getColumnsByUserId,
+};
