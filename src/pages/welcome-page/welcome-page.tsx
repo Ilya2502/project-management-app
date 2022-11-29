e7be54fcca8fc038497765ebe69e95226f35ff84
@Mike-Prybytkin <prybytkin@bk.ru>
2022-11-30 00:52:01 +0300

    feat: add our team block

--- src/pages/welcome-page/welcome-page.tsx
+++ src/pages/welcome-page/welcome-page.tsx
@@ -2,17 +2,64 @@ import React from 'react';
 
 const WelcomePage = () => {
   return (
-    <div className="welcome-page">
-      <div className="welcome-page__title">
-        <h1>Kanban</h1>
-        <p>
-          Our app is designed to help you get more done. We are constantly updating our software to
-          make it easier and easier for your teams to complete their tasks, and for you to keep
-          track of what is happening in your company.
-        </p>
+    <div className="welcome-page-wrapper">
+      <div className="welcome-page">
+        <div className="welcome-page__title">
+          <h1>Kanban</h1>
+          <p>
+            Our app is designed to help you get more done. We are constantly updating our software
+            to make it easier and easier for your teams to complete their tasks, and for you to keep
+            track of what is happening in your company.
+          </p>
+        </div>
+        <div className="welcome-page__img">
+          <img src="assets/manage-platform.png" alt="manage-platform" />
+        </div>
       </div>
-      <div className="welcome-page__img">
-        <img src="assets/manage-platform.png" alt="manage-platform" />
+      <div className="our-team">
+        <h2 className="our-team__title">Our team</h2>
+        <div className="teammates">
+          <div className="teammates-person">
+            <img className="teammates-person__photo" src="assets/team/mihail.png" alt="mihail" />
+            <div className="teammates-person__about">
+              <h3 className="teammates-person__name">Mihail</h3>
+              <p className="teammates-person__position">Front-end developer</p>
+              <a
+                href="https://www.linkedin.com/in/prybytkin-mike/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                LinkedIn
+              </a>
+            </div>
+          </div>
+
+          <div className="teammates-person">
+            <img className="teammates-person__photo" src="assets/team/ilya.png" alt="ilya" />
+            <div className="teammates-person__about">
+              <h3 className="teammates-person__name">Ilya</h3>
+              <p className="teammates-person__position">Team Lead</p>
+              <a href="https://www.linkedin.com/" target="_blank" rel="noreferrer">
+                LinkedIn
+              </a>
+            </div>
+          </div>
+
+          <div className="teammates-person">
+            <img className="teammates-person__photo" src="assets/team/vitaliy.png" alt="vitaliy" />
+            <div className="teammates-person__about">
+              <h3 className="teammates-person__name">Vitaliy</h3>
+              <p className="teammates-person__position">Front-end developer</p>
+              <a
+                href="https://www.linkedin.com/in/vitaliksamusenko/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                LinkedIn
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
